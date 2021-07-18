ce90c342db718d60b6d94efdb7192edee99f589f
PolGs <polgalvezsoriano@gmail.com>
2021-07-18 06:20:45 +0200

    Add files via upload

---
+++ ccs/cjt_idioma.cc
@@ -0,0 +1,28 @@
+
+/** @file
+ * Implementación del codificador.
+ */
+
+#include "cjt_idioma.hh"
+
+/** @class cjt_idioma
+	@brief Representa un conjunto de idiomas.
+	
+	Ofereix operacions de lectura de l'experiment, escriptura i consultes d'un individu.
+*/
+
+
+
+void cjt_idioma::cjt_idioma(){
+    
+}
+void cjt_idioma::~cjt_idioma(){
+    
+}
+
+void cjt_idioma::afegir_idioma(idioma i){
+    
+}
+
+
+
---
+++ ccs/idioma.cc
@@ -0,0 +1,30 @@
+
+/** @file
+ * Implementación del codificador.
+ */
+
+/** @class idioma
+	@brief Representa el conjunt d'individus d'un experiment.
+	
+	Ofereix operacions de lectura de l'experiment, escriptura i consultes d'un individu.
+*/
+
+
+
+idioma::idioma() {}
+
+idioma::~idioma() {}
+
+
+void idioma::modificar_idioma(){}
+
+tabla idioma::consultar_tabla(){}
+
+treecode idioma::consultar_treecode(){}
+
+void consultar_codigos(){}
+
+
+
+
+
